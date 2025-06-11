35d05ae719a0741d66a51ed984a64014a5162bb2
Jakub Hrubý <hrubyy.jakub@gmail.com>
2025-06-11 18:24:27 +0200

    [ROOT][master] - added env variables type safety and validation

--- src/common/constants/env/index.ts
+++ src/common/constants/env/index.ts
@@ -1,26 +1,54 @@
-export const ENV_CONFIG_PUBLIC = Object.freeze({
-    ENV: process.env.NEXT_PUBLIC_ENV,
-    API_URL: process.env.NEXT_PUBLIC_API_URL,
-    GOOGLE_OAUTH_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID,
-    ORIGIN: process.env.NEXT_PUBLIC_ORIGIN,
-    COOKIE_DOMAIN: process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
-    GOOGLE_STORAGE_BUCKET_RECIPE_IMAGES:
-        process.env.NEXT_PUBLIC_GOOGLE_STORAGE_BUCKET_RECIPE_IMAGES
-});
+function createConfig<T extends Record<string, string | undefined>>(
+    config: T,
+    configName: string,
+    isValidationEnabled: boolean
+): Readonly<{ [K in keyof T]: string }> {
+    if (isValidationEnabled) {
+        for (const [key, value] of Object.entries(config)) {
+            if (!value) {
+                throw new Error(
+                    `Missing environment variable ${key} in ${configName}`
+                );
+            }
+        }
+    }
 
-export const ENV_CONFIG_PRIVATE = Object.freeze({
-    DB_NAME: process.env.DB_NAME,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_PORT: process.env.DB_PORT,
-    DATABASE_URL: process.env.DATABASE_URL,
-    DB_CONNECTIONS: process.env.DB_CONNECTIONS,
-    DB_IDLE_TIMEOUT: process.env.DB_IDLE_TIMEOUT,
-    DB_ACQUIRE_TIMEOUT: process.env.DB_ACQUIRE_TIMEOUT,
-    DB_MAX_ATTEMPTS: process.env.DB_MAX_ATTEMPTS,
-    GOOGLE_SMTP_USERNAME: process.env.GOOGLE_SMTP_USERNAME,
-    GOOGLE_SMTP_PASSWORD: process.env.GOOGLE_SMTP_PASSWORD,
-    JWT_SECRET: process.env.JWT_SECRET,
-    JWT_TOKEN_DURATION_DAYS: process.env.JWT_TOKEN_DURATION_DAYS,
-    JWT_ISSUER: process.env.JWT_ISSUER
-});
+    return Object.freeze(config as { [K in keyof T]: string });
+}
+
+export const ENV_CONFIG_PUBLIC = createConfig(
+    {
+        ENV: process.env.NEXT_PUBLIC_ENV,
+        API_URL: process.env.NEXT_PUBLIC_API_URL,
+        GOOGLE_OAUTH_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID,
+        ORIGIN: process.env.NEXT_PUBLIC_ORIGIN,
+        COOKIE_DOMAIN: process.env.NEXT_PUBLIC_COOKIE_DOMAIN,
+        GOOGLE_STORAGE_BUCKET_RECIPE_IMAGES:
+            process.env.NEXT_PUBLIC_GOOGLE_STORAGE_BUCKET_RECIPE_IMAGES
+    },
+    'ENV_CONFIG_PUBLIC',
+    process.env.NEXT_PUBLIC_ENV !== 'test'
+);
+
+export const ENV_CONFIG_PRIVATE = createConfig(
+    {
+        DB_NAME: process.env.DB_NAME,
+        DB_USERNAME: process.env.DB_USERNAME,
+        DB_PASSWORD: process.env.DB_PASSWORD,
+        DB_PORT: process.env.DB_PORT,
+        DATABASE_URL: process.env.DATABASE_URL,
+        DB_CONNECTIONS: process.env.DB_CONNECTIONS,
+        DB_IDLE_TIMEOUT: process.env.DB_IDLE_TIMEOUT,
+        DB_ACQUIRE_TIMEOUT: process.env.DB_ACQUIRE_TIMEOUT,
+        DB_MAX_ATTEMPTS: process.env.DB_MAX_ATTEMPTS,
+        GOOGLE_SMTP_USERNAME: process.env.GOOGLE_SMTP_USERNAME,
+        GOOGLE_SMTP_PASSWORD: process.env.GOOGLE_SMTP_PASSWORD,
+        JWT_SECRET: process.env.JWT_SECRET,
+        JWT_TOKEN_DURATION_DAYS: process.env.JWT_TOKEN_DURATION_DAYS,
+        JWT_ISSUER: process.env.JWT_ISSUER,
+        GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+        GOOGLE_OAUTH_REDIRECT_URI: process.env.GOOGLE_OAUTH_REDIRECT_URI
+    },
+    'ENV_CONFIG_PRIVATE',
+    process.env.NEXT_PUBLIC_ENV !== 'test' && typeof window === 'undefined'
+);
