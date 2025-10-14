e7dfaf5163f4f705b793d4b0489d81edaba9f07b
Jakub Hrubý <hrubyy.jakub@gmail.com>
2025-10-14 19:15:03 +0200

    [CLIENT][socials] - added socoal network icons

--- src/client/components/atoms/Icons/svg/socials/index.ts
+++ src/client/components/atoms/Icons/svg/socials/index.ts
@@ -1,5 +1,15 @@
 import GoogleIcon from './google.svg';
+import FacebookIcon from './facebook.svg';
+import TwitterIcon from './twitter.svg';
+import WhatsappIcon from './whatsapp.svg';
+import EmailIcon from './email.svg';
+import ShareIcon from './share.svg';
 
 export const socialsSvg = {
-    GoogleIcon
+    GoogleIcon,
+    FacebookIcon,
+    TwitterIcon,
+    WhatsappIcon,
+    EmailIcon,
+    ShareIcon
 };
